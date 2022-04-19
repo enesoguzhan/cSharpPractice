a663d18df85021548b5cf2f29f1964f30e53da71
Enes Oğuzhan ARACI <earaci@ozatashipyard.com>
2022-04-19 10:22:20 +0300

    Add project files.

---
+++ cSharpPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
